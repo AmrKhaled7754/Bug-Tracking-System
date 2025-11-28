b213494f49036de21487f57a748d54d91c1651cc
Amr8545 <omarkhaledbdwy2006@gmail.com>
2025-11-29 01:16:57 +0200

    Initial project structure

---
+++ Main/Main.java
@@ -0,0 +1,17 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX is working!");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
