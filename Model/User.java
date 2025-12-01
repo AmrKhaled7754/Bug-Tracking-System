b2029d464a790398d4971066e3a9961a752e59ab
Amr <amrkhaledbdwy@gmail.com>
2025-12-01 03:39:34 +0200

    adding users data adding user structure

--- Model/User.java
+++ Model/User.java
@@ -12,18 +12,42 @@ public abstract class User {
     }
 
     // Getters and Setters
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-    public String getRole() { return role; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    // Abstract method - each role implements differently
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getPassword() {
+        return password;
+
+
+    }
+
+    // Abstract method
     public abstract void displayMenu();
 
     @Override
