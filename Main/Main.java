bcf68c8ba63cf69a0cc0dafb1b05e99995f32810
AmrKhaled7754 <amrkhaledbdwy@gmail.com>
2025-12-10 11:52:56 +0200

    Remove Main class and JavaFX application code

--- Main/Main.java
+++ Main/Main.java
@@ -1,17 +1,1 @@
-package Main;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX is working!");
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
